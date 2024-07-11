197fdd2e55dd0dfede41f2cce61baf56532a2a91
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-07-11 08:56:39 -0500

    Created a default and production file that will be used for the deployment of the API.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Testing Testing");
+require('dotenv').config
+import express from 'express'
+import config from 'config'
+
+const app = express()
+
+const port = config.get('port')
+
+app.listen(port, () => [
+    console.log(`App started at https://localhost:${port}`),
+]);
