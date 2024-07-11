d3a0fafc0ffbb9dcd2c4e43a44282059fb148f38
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-07-11 09:03:17 -0500

    Initialize Express server with enviorment configurations and dynamic port setup.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
+// Loading enviorment variables from a .env file into process.env
 require('dotenv').config
+
+//Importing the Express framework for building wen applications
 import express from 'express'
+
+//Importing the config module to manage application configurations
 import config from 'config'
 
+// Created an instance of an Express application 
 const app = express()
 
+//Retrieved the port number from the configuration settings
 const port = config.get('port')
 
+// Here I started the express server and listen on the specified port
 app.listen(port, () => [
+    //printing out a message to the terminal showing that the server does indeed work
     console.log(`App started at https://localhost:${port}`),
 ]);
