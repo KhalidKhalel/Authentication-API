e0ac00ea67a15aa8cfc6a2824e4c15734fdc6774
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-08-06 17:55:08 -0500

    Added nodemailer setup for creating test credentials

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,12 +1,16 @@
 import {Request, Response} from "express"
 import { CreateUserInput } from "../schema/user.schema"
 import { createUser } from "../service/user.service";
+import sendEmail from "../utils/mailer";
+
 
 export async function createUserHandler(req: Request<{}, {}, CreateUserInput>,res: Response){
     const body = req.body;
 
     try{
         const user = await createUser(body);
+
+        await sendEmail();
 
         return res.send("User successfully created");
     }catch(e:any){
