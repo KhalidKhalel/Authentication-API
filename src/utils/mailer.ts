e2a2b13847b2d5fb87cca1e2884d1667693b860b
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-08-07 10:51:56 -0500

    Configured SMTP settings from config file and Added sendEmail function to send emails and log errors or preview URL

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,14 +1,26 @@
-import nodemailer from 'nodemailer'
+import nodemailer, {SendMailOptions} from 'nodemailer'
+import config from 'config'
+import log from './logger';
 
-async function createTestCreds(){
-    const creds = await nodemailer.createTestAccount()
-    console.log( {creds});
-}
+const smtp = config.get<{
+    user: string,
+    pass: string,
+    host: string,
+    post: number,
+    secure: boolean
+}>('smtp');
 
-createTestCreds();
-
-async function sendEmail(){
-
+const transporter = nodemailer.createTransport({
+    ...smtp,
+    auth: {user: smtp.user, pass: smtp.pass}
+})
+async function sendEmail(payload: SendMailOptions ){
+    transporter.sendMail(payload, (err, info) => {
+        if(err){
+            log.error(err, "Error sending email")
+        }
+        log.info(`Preview URL:  ${nodemailer.getTestMessageUrl(info)}`)
+    })
 }
 
 export default sendEmail;
