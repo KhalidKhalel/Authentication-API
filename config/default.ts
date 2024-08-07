b90a1b1b01fbd8ac0dd379ac34a3571b9436e5ef
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-08-07 10:33:16 -0500

    Added SMTP configuration for email service

--- config/default.ts
+++ config/default.ts
@@ -2,5 +2,11 @@ export default {
     port: 3000,
     dbUri: "mongodb://localhost:27017/Authentication-API",
     logLevel: "info",
-
+    smtp:{
+        user: 'uziwvaaoqve3wkjl@ethereal.email',
+        pass: 'mrZSVkG4HkW88xgSeU',
+        host: 'smtp.ethereal.email',
+        port: 587,
+        secure: false,
+    }
 }
