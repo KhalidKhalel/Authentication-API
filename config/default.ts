bbb663822e11b9572e5c3e088e7c7be287bf43b3
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-08-14 12:20:45 -0500

    Added JWT signing utility function and config for token keys

--- config/default.ts
+++ config/default.ts
@@ -2,6 +2,8 @@ export default {
     port: 3000,
     dbUri: "mongodb://localhost:27017/Authentication-API",
     logLevel: "info",
+    accessTokenPrivateKey: "",
+    refreshTokenPrivateKey: "",
     smtp:{
         user: 'uziwvaaoqve3wkjl@ethereal.email',
         pass: 'mrZSVkG4HkW88xgSeU',
