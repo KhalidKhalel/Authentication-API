1e61e582e2dc287c9d91b5bf93f26d5a23224a12
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-08-14 12:34:20 -0500

    Created JWT verification utility function

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -17,3 +17,18 @@ export function signJwt(
       });
 }
 
+export function verifyJwt<T>(
+    token: string,
+    keyName: "accessTokenPublicKey" | "refreshTokenPublicKey"
+  ): T | null {
+    const publicKey = Buffer.from(config.get<string>(keyName), "base64").toString(
+      "ascii"
+    );
+  
+    try {
+      const decoded = jwt.verify(token, publicKey) as T;
+      return decoded;
+    } catch (e) {
+      return null;
+    }
+  }
