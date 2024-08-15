17929c6501c1bc7c4194cb640b7d2608a2f18123
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-08-15 12:26:20 -0500

    Created a createSession Function that will create a new session for a user.

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,6 +1,11 @@
 import { DocumentType } from "@typegoose/typegoose";
 import { User } from "../model/user.model";
 import { signJwt } from "../utils/jwt";
+import SessionModel from "../model/session.model";
+
+export async function createSession({ userId }: { userId: string }) {
+    return SessionModel.create({ user: userId });
+}
 
 export function signAccessToken(user: DocumentType<User>){
     const payload = user.toJSON()
